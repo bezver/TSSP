60c65fcd882806e58ba7ff663081ba0f4f7a7014
bezver <60857455+bezver@users.noreply.github.com>
2021-05-09 16:56:58 +0300

    Done autorization

---
+++ Kandidaten/Autorization.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kandidaten
+{
+
+    class AutorizationException : Exception
+    {
+        public AutorizationException() { }
+
+        public AutorizationException(string message) : base(message) { }
+    }
+
+    static class Autorization
+    {
+        public static bool isAutorization { get; private set; }
+
+        private static string username_ = "admin";
+        private static string password_ = "admin";
+
+        public static void Login(string username, string password)
+        {
+            if (username != username_ || password != password_)
+            {
+                throw new AutorizationException("Wrong username or password");
+            }
+            isAutorization = true;
+        }
+
+        public static void Logout()
+        {
+            isAutorization = false;
+        }
+    }
+}
