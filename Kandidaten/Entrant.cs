ac89706f6bcb72f46f01326e6ff4b55d5b76fd46
bezver <60857455+bezver@users.noreply.github.com>
2021-05-09 17:40:56 +0300

    done Entrant

---
+++ Kandidaten/Entrant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kandidaten
+{
+    class Entrant
+    {
+        public string firstName   { get; private set; }
+        public string secondName  { get; private set; }
+        public double averageMark { get; private set; }
+        public uint school        { get; private set; }
+
+        public Entrant(string firstName_, string secondName_, double averageMark_, uint school_) {
+            firstName   = firstName_;
+            secondName  = secondName_;
+            averageMark = averageMark_;
+            school      = school_;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{firstName} {secondName}, середній бал: {averageMark}, школа: {school}");
+        }
+
+    }
+}
